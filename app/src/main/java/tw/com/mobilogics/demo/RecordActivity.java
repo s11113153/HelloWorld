e68bcbdfa851f1891e165636ea6caaf09b6cf29e
didi <didi@mobilogics.com.tw>
2014-12-15 01:31:36 +0800

    add total today's calories

--- app/src/main/java/tw/com/mobilogics/demo/RecordActivity.java
+++ app/src/main/java/tw/com/mobilogics/demo/RecordActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -13,6 +14,7 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import tw.com.mobilogics.demo.db.DBHelper;
 
@@ -31,6 +33,7 @@ public class RecordActivity extends Activity {
 
   private List<String> checkData = new ArrayList();
 
+  private static HashMap<String, Integer> total = new HashMap();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -45,20 +48,29 @@ public class RecordActivity extends Activity {
       if (!checkData.contains(date)) {
         checkData.add(date);
         TextView textView = new TextView(this);
-        textView.setTextSize(32);
+        textView.setTextSize(20);
         textView.setBackgroundColor(Color.YELLOW);
         textView.setText(date);
         mViewList.add(textView);
       }
+
       TextView textView = new TextView(this);
       String msg = "時間" + time.split(" ")[1] + ",  熱量";
       if (calories <= 0) msg += "消耗 " + calories;
       else msg += "增加 " + calories;
+      if (!total.containsKey(date)) {
+        total.put(date, 0);
+      }
+      int sum = (total.get(date)) + calories;
+      total.put(date, sum);
       textView.setText(msg);
       mViewList.add(textView);
     }
     cursor.close();
     mListView.setAdapter(new RecordAdapter(mViewList));
+    for (Map.Entry entry : total.entrySet()) {
+      Log.e("" + entry.getKey(), "" + entry.getValue());
+    }
   }
 
   public static class RecordAdapter extends BaseAdapter {
@@ -86,6 +98,13 @@ public class RecordActivity extends Activity {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+      View view = mViews.get(position);
+      if (view instanceof TextView) {
+        String title = ((TextView) view).getText().toString();
+        if (total.get(title)!= null && total.get(title) != 0) {
+          ((TextView) view).setText(title + ", 今日熱量 : " + total.get(title));
+        }
+      }
       return mViews.get(position);
     }
   }
